68452cb65a3af4046d80d5dd3b52521c8b5f152d
mario <mario.mazzarelli@asfweb.net>
2022-12-15 13:02:46 +0100

    DONE

--- www/NativeOauthIds.js
+++ www/NativeOauthIds.js
@@ -1,5 +1,5 @@
 var exec = require("cordova/exec");
 
 exports.login = function (url, success, error) {
-  exec(success, error, "NativeOauthIds", "onMessageReceived", [url]);
+  exec(success, error, "NativeOauthIds", "login", [url]);
 };
