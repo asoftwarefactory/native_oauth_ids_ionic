df23da87df494af5565c7b4fab2b035bdbf14320
mariomazz <74057624+mariomazz@users.noreply.github.com>
2022-12-16 21:38:57 +0100

    Update NativeOauthIds.js
    Fix

--- www/NativeOauthIds.js
+++ www/NativeOauthIds.js
@@ -1,19 +1,4 @@
 (function () {
-  /* var exec = require("cordova/exec");
-  var channel = require("cordova/channel"); */
-
-  /* function NativeOauthIds() {
-    this.channels = {
-      login: channel.create("login"),
-    };
-  }
-
-  NativeOauthIds.prototype.login = function (url, success, error) {
-    exec(success, error, "NativeOauthIds", "login", [url]);
-  };
-
-  module.exports = new NativeOauthIds(); */
-
   var exec = require("cordova/exec");
   // var channel = require("cordova/channel");
 
