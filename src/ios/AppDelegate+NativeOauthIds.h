e3748347886de4198bc22b6691c89c5ffc03966a
mario <mario.mazzarelli@asfweb.net>
2022-12-20 15:54:22 +0100

    FIX

--- src/ios/AppDelegate+NativeOauthIds.h
+++ src/ios/AppDelegate+NativeOauthIds.h
@@ -1,11 +1,10 @@
 #import "AppDelegate.h"
-@import NativeOauthIds;
 
 @interface AppDelegate (NativeOauthIds)
 
-- (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
+- (BOOL)application:(UIApplication *_Nullable)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *_Nullable)launchOptions;
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+- (BOOL)application:(UIApplication *_Nullable)app openURL:(NSURL *_Nullable)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *_Nullable)options;
 
 + (AppDelegate *_Nonnull) instance;
 
